abd92057746360eb6197396a09b4dfe2e5dc44ef
Edward-Cristi Butoiu <butoiu.edwardcristi@outlook.com>
2019-10-15 00:52:11 +0300

    filtering + using current userId as filter if no idObserver is provided

--- src/api/VoteMonitor.Api.Note/Handlers/NoteQueryHandler.cs
+++ src/api/VoteMonitor.Api.Note/Handlers/NoteQueryHandler.cs
@@ -8,26 +8,45 @@ using Microsoft.EntityFrameworkCore;
 using VoteMonitor.Entities;
 using VoteMonitor.Api.Note.Models;
 using VoteMonitor.Api.Note.Queries;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System;
 
 namespace VoteMonitor.Api.Note.Handlers
 {
-    public class NoteQueryHandler :
-        IRequestHandler<NoteQuery, List<NoteModel>>
+    public class NoteQueryHandler : IRequestHandler<NoteQuery, List<NoteModel>>
     {
 
         private readonly VoteMonitorContext _context;
         private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContext;
 
-        public NoteQueryHandler(VoteMonitorContext context, IMapper mapper)
+        public NoteQueryHandler(VoteMonitorContext context, IMapper mapper, IHttpContextAccessor httpContext)
         {
             _context = context;
             _mapper = mapper;
+            _httpContext = httpContext;
         }
+
         public async Task<List<NoteModel>> Handle(NoteQuery message, CancellationToken token)
         {
-            return await _context.Notes
-                .Where(n => n.IdObserver == message.IdObserver && n.IdPollingStation == message.IdPollingStation)
+            var parsed = int.TryParse(_httpContext.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value, out int userId);
+            if (!parsed)
+                throw new Exception($"No authentication context !");
+
+            var query = _context.Notes.AsQueryable();
+
+            if (message.IdObserver.HasValue)
+                query = query.Where(x => x.IdObserver == message.IdObserver);
+            else
+                query = query.Where(x => x.IdObserver == userId);
+
+            if (message.IdPollingStation.HasValue)
+                query = query.Where(x => x.IdPollingStation == message.IdPollingStation);
+
+            return await query
                 .OrderBy(n => n.LastModified)
+                .AsNoTracking()
                 .Select(n => new NoteModel
                 {
                     NoteAttachments = n.NoteAttachments.Select(x => x.NotePath).ToList(),
